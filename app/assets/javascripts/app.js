04576f27f17f104222001b35783dd2511a2a46cf
Brian Ethier <davidbrianethier@gmail.com>
2015-12-28 23:02:26 -0500

    Adding ui router setup, getting rid of the hash marks in urls

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,6 +5,22 @@ var pjApp = angular.module('pjApp', [
     'ng-rails-csrf'
 ]);
 
+
+pjApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  $urlRouterProvider.otherwise("/");
+  $locationProvider.html5Mode(true);
+
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "assets/home.html"
+    })
+    .state('login', {
+      url: "/login",
+      templateUrl: "assets/login.html"
+    });
+});
+
 pjApp.controller('pjController', ['$scope', '$resource', 'User', 'Item', 'Sale', function($scope, $resource, User, Item, Sale) {
 
   //TODO: I can take out the logging and the error handler. Just for debugging now
