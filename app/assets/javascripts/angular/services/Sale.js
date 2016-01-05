5aa04b2b900258b85862821a7fe3d6b9914be139
Brian Ethier <davidbrianethier@gmail.com>
2016-01-05 01:04:05 -0500

    Adding util service to factor out common code for non rest functions

--- app/assets/javascripts/angular/services/Sale.js
+++ app/assets/javascripts/angular/services/Sale.js
@@ -1,23 +1,21 @@
-angular.module('pjApp').factory('Sale', ['restmod', '$q', function(restmod, $q) {
-  return restmod.model('/api/v1/sales').mix({
-    $extend: {
-      Model: {
-        current: function() {
-          var instance = new this();
-          return instance.$send({
-            url: this.$url() + "/current",
-            method: 'GET'
-          }).$then(function(o) {
-            var data = o.$response.data;
+(function() {
+  'use strict'
 
-            if(data === null || data.id === undefined) {
-              return $q.reject("No sales were found.");
-            } else {
-              this.$decode(data);
-            }
-          });
+  angular.
+    module('pjApp').
+    factory('Sale', Sale);
+
+  Sale.$inject = ['restmod', 'utilService'];
+
+  function Sale(restmod, utilService) {
+
+    return restmod.model('/api/v1/sales').mix({
+      $extend: {
+        Model: {
+          current: utilService.restMod.nonRest('/current')
         }
       }
-    }
-  });
-}]);
+    });
+
+  }
+})();
---
+++ app/assets/javascripts/angular/services/util-service.js
@@ -0,0 +1,44 @@
+(function() {
+  'use strict'
+
+  angular.
+    module('pjApp').
+    factory('utilService', utilService);
+
+  utilService.$inject = ['$q'];
+
+  function utilService($q) {
+
+    //Calls a url endpoint that lies outside of normal rest methods.
+    //As of now only does a get request
+    //Meant for extension of RestMod models
+    function nonRest(urlSuffix) {
+
+      return function() {
+        //This function gets called in the context of the resource it will be fetching. Thus new this() works.
+        var instance = new this();
+
+        return instance.$send({
+          url: this.$url() + urlSuffix,
+          method: 'GET'
+        }).$then(function(o) {
+          var data = o.$response.data;
+
+          if(data === null || data.id === undefined) {
+            return $q.reject("None found.");
+          } else {
+            this.$decode(data);
+          }
+        });
+      }
+
+    }
+
+    return {
+      restMod : {
+        nonRest : nonRest
+      }
+    }
+  }
+
+})();
