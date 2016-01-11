55bf0d882c048e147ae29eb158755e64acbf561f
Brian Ethier <davidbrianethier@gmail.com>
2016-01-11 00:24:34 -0500

    Adding in Item again

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111052046) do
+ActiveRecord::Schema.define(version: 20160111052404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name",        null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "sales", force: :cascade do |t|
     t.datetime "start",      null: false
